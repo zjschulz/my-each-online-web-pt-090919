6038dd44019c4c74a2dfc1859716799c593aaf30
Zachary Schulz <zjschulz@gmail.com>
2019-09-20 00:18:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
 def my_each(words)
-  
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
